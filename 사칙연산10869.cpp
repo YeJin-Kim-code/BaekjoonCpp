bb48a0addc40db213d8fae86225ed9defcdb3181
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2023-09-23 23:17:26 +0900

    Add files via upload

---
+++ 사칙연산10869.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int result = 0;
+	cin >> a;
+	cin >> b;
+	result = a + b;
+
+	cout << result << endl;
+
+	result = a - b;
+
+	cout << result << endl;
+
+	result = a * b;
+
+	cout << result << endl;
+
+	result = a / b;
+
+	cout << result << endl;
+
+	result = a % b;
+
+	cout << result << endl;
+
+}
