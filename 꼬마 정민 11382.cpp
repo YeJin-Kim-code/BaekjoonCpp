1bb13a3bdefe4573570108b82c1833fb807d3d35
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2023-10-04 00:54:16 +0900

    Add files via upload

---
+++ 꼬마 정민 11382.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long a, b, c = 0;
+
+	cin >> a >> b >> c;
+
+	cout << a + b + c;
+
+}
